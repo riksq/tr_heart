66e00c4969c8dcfc0ec490c1b3b6c85dfd71b005
riksq <91621987+riksq@users.noreply.github.com>
2021-09-30 00:13:37 +0300

    Update hello.js
    21

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,5 +2,5 @@
 
 export default function handler(req, res) {
     // res.status(200).json({ ok: true, url: "https://instaprix17.cam?a_aid=os&a_bid=ca7b841d" });
-    return res.status(200).json({ ok: true, url: "https://google.com" });
+    return res.status(200).json({ ok: true, url: "https://www.helloheart.com" });
 }
