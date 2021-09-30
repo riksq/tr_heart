019ff45ad558e96938c1bc2bf31937954df0a61a
riksq <91621987+riksq@users.noreply.github.com>
2021-09-30 16:04:15 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -51,7 +51,7 @@ export default function Home(props) {
        // alert(gtm);
         // TagManager.initialize({ gtmId: "GTM-PTZV39P" });
         // TagManager.initialize({ gtmId: "GTM-59WP5XJ" });
-        TagManager.initialize({ gtmId: "GTM-PMT8CQR" });
+        TagManager.initialize({ gtmId: "GTM-T62N69R" });
        // if (gtm) {
           //  TagManager.initialize({ gtmId: gtm });
        // }
