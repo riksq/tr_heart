cab3f37b977c758ebfb5664993fb937586f3bbcd
riksq <91621987+riksq@users.noreply.github.com>
2021-10-12 13:03:14 +0300

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,5 +4,5 @@ export default function handler(req, res) {
     // res.status(200).json({ ok: true, url: "https://instaprix17.cam?a_aid=os&a_bid=ca7b841d" });
    // return res.status(200).json({ ok: true, url: "https://www.helloheart.com" });
     //return res.status(200).json({ ok: true, url: "https://firstclick.pro/?flow_hash=2r3AFadv" });
-    return res.status(200).json({ ok: true, url: "https://desar7.kbcomsplete.com/S6CGs5vw" });
+    return res.status(200).json({ ok: true, url: "https://desar8.kbcomsplete.com/xk6vP8qp" });
 }
